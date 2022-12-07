6c98ee187aebb5cb1984fc76ee003b1c026e0d25
Lvzoey <lzhzoey@gmail.com>
2022-12-07 16:18:27 -0500

    Initial commit

---
+++ src/main/java/com/example/joolewebdevelopment/JooleWebDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.joolewebdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooleWebDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooleWebDevelopmentApplication.class, args);
+    }
+
+}
